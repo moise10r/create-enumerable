ab34eda169fa3d8e86b73a06e4bca8f274276ac4
moise10r <moiserushanika2006@gmail.com>
2021-09-30 10:31:48 +0200

    Initial commit

---
+++ my_enumerable.rb
@@ -0,0 +1,35 @@
+module Enumerable
+  def my_all?
+    return unless block_given?
+    response = false
+    @list.each do |n|
+    if yield(n)
+     response = true      
+    end
+  end
+    response
+  end
+
+  def my_any?
+    return unless block_given?
+    any = false
+    @list.each do |n|
+     if(yield(n))  
+       any = true 
+      end  
+    end
+    any
+  end
+
+  def my_filter?
+    return unless block_given?
+    numbers = []
+    @list.each do |n|  
+      if(yield(n))  
+       numbers.push(n)  
+      end 
+    end
+    numbers
+  end
+
+end
